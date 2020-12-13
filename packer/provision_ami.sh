2489bca18d2dce02a945635b45d151eb0e6d9eda
Ninad Pundalik <ninadsp16289+github@gmail.com>
2020-12-14 00:18:07 +0530

    Working firewall config

--- packer/provision_ami.sh
+++ packer/provision_ami.sh
@@ -6,7 +6,17 @@
 set -ex
 
 sudo apt-get update -q
-sudo apt-get install -qy fail2ban wireguard
+sudo apt-get install -qy linux-headers-$(uname -r) wireguard nftables
 
 sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
-# TODO: Configure fail2ban
+
+ifname=$(ip link | awk -F: '$0 !~ "lo|vir|wl|wg|^[^0-9]"{print $2;getline}' | tr -d ' ')
+
+sudo nft add table ip nat
+sudo nft add chain ip nat PREROUTING { type nat hook prerouting priority 0 \; }
+sudo nft add chain ip nat POSTROUTING { type nat hook postrouting priority 100 \; }
+sudo nft add rule ip nat POSTROUTING ip saddr 10.0.0.0/24 oifname "${ifname}" masquerade
+
+sudo /bin/bash -c "nft list ruleset >> /etc/nftables.conf"
+sudo systemctl enable nftables
+sudo systemctl start nftables
