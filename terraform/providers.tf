86f6613f58c047340d984c44a3c39c4e6a45e735
Ninad Pundalik <ninadsp16289+github@gmail.com>
2021-04-17 15:55:12 +0530

    tf 0.13 upgrade

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  version = "~> 2.0"
   region  = var.region
 }
 
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,3 +1,15 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.0"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
 }
