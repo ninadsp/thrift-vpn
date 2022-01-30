f8d4f6dd393536d293f183ec7049e7ee87b64d2c
Jérôme Le Gal <jlegal@iobeya.com>
2022-01-30 13:56:37 +0000

    Use templatefile() instead of template_file (+ Mac M1 support)
    Thanks a lot for sharing this project, I had some fun playing with it :)
    
    This PR adds Mac M1 support by removing an unavailable provider for
    Mac M1, and updating an another one.
    
    First, the `template_file` data source is no longer supported and a new
    function `templatefile()` is available since terraform 0.12. So the
    provider `hashicorp/template` has been deleted, and code using
    `template_file` has been updated and now use `templatefile()`.
    
    Secondly, the `hashicorp/aws` constraint version has been updated to
    allow 3.0 versions, as 2.0 verions are not available for Mac ARM
    architecture.
    
    These modifications have been tested using an empty AWS account.
    
    Thanks again!

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,15 +1,13 @@
 terraform {
   required_version = ">= 0.14"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.0"
+      version = "~> 3.0"
     }
     null = {
       source = "hashicorp/null"
-    }
-    template = {
-      source = "hashicorp/template"
     }
   }
 }
